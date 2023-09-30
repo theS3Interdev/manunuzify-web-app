fd204ea080e26d87c3205fd30d97faf227c9d2b7
Michael Owuor <michael.owuor@gmail.com>
2023-10-01 00:02:59 +0300

    feat: create ui layout

--- components/index.js
+++ components/index.js
@@ -1,19 +1,23 @@
 import App from "@/components/app";
+import AppHeader from "@/components/app-header";
+import AppSidebar from "@/components/app-sidebar";
 import CheckoutWizard from "@/components//checkout-wizard";
 import Container from "@/components/container";
-import DisplayImage from "@/components/display-image";
-import Header from "@/components/header";
+import ImageDisplay from "@/components/image-display";
 import ProductCard from "@/components/product-card";
 import ProductList from "@/components/product-list";
 import ProductRating from "@/components/product-rating";
+import UserProfileButton from "@/components/user-profile-button";
 
 export {
   App,
+  AppHeader,
+  AppSidebar,
   CheckoutWizard,
   Container,
-  DisplayImage,
-  Header,
+  ImageDisplay,
   ProductCard,
   ProductList,
   ProductRating,
+  UserProfileButton,
 };
