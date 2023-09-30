49998fab607eb521e2112908cbb07c58ec306f7c
Michael Owuor <michael.owuor@gmail.com>
2023-10-01 02:31:28 +0300

    feat: Implement add to cart functionality (#5)
    * feat: add redux  state management
    
    * feat: implement add to cart functionality

--- components/app-sidebar.jsx
+++ components/app-sidebar.jsx
@@ -1,8 +1,10 @@
 "use client";
 
 import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+import { Menu, ShoppingBag, ShoppingCart, Trash2 } from "lucide-react";
 
-import { Menu } from "lucide-react";
+import { addToCart, removeFromCart } from "@/lib/redux/slices/cart-slice";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -15,8 +17,21 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { ImageDisplay } from "@/components/index";
 
 const AppSidebar = () => {
+  const { cartItems, itemsPrice, loading } = useSelector((state) => state.cart);
+
+  const dispatch = useDispatch();
+
+  const addToCartHandler = (product, quantity) => {
+    dispatch(addToCart({ ...product, quantity }));
+  };
+
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id));
+  };
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -30,12 +45,84 @@ const AppSidebar = () => {
           </SheetDescription>
         </SheetHeader>
         {/* cart items display start */}
-        <p className="py-5">Your shopping cart is empty.</p>
+        <div>
+          {loading ? (
+            <p className="py-5">Loading...</p>
+          ) : cartItems.length === 0 ? (
+            <p className="py-5">Your cart is empty.</p>
+          ) : (
+            <div>
+              <div className="flex flex-col border-b p-2">
+                <div className="flex justify-between space-x-2">
+                  <p className="font-montserrat font-semibold">Subtotal</p>
+                  <p className="font-semibold">
+                    {itemsPrice.toLocaleString("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    })}
+                  </p>
+                </div>
+
+                <SheetClose asChild>
+                  <Button asChild className="mb-2 mt-5 w-full">
+                    <Link href="/cart">
+                      <ShoppingCart className="mr-2 h-4 w-4" />
+                      Go to Cart
+                    </Link>
+                  </Button>
+                </SheetClose>
+              </div>
+
+              {cartItems.map((item) => (
+                <div key={item.id} className="flex flex-col items-center p-1">
+                  <div className="relative m-2 aspect-square h-40 w-40 rounded-lg bg-foreground/5 dark:bg-background">
+                    <Link href={`/product/${item.id}`}>
+                      <ImageDisplay
+                        imageSrc={item.image}
+                        imageAlt={item.name}
+                      />
+                    </Link>
+                  </div>
+
+                  <div className="mb-2">
+                    <p className="text-center font-semibold">{item.name}</p>
+                  </div>
+
+                  <select
+                    value={item.quantity}
+                    onChange={(e) =>
+                      addToCartHandler(item, Number(e.target.value))
+                    }
+                    className="w-40"
+                  >
+                    {Array.from(Array(item.quantityInStock).keys()).map((i) => (
+                      <option key={i + 1} value={i + 1}>
+                        {i + 1}
+                      </option>
+                    ))}
+                  </select>
+
+                  <Button
+                    onClick={() => removeFromCartHandler(item.id)}
+                    variant="destructive"
+                    className="mt-3 w-full"
+                  >
+                    <Trash2 className="mr-2 h-4 w-4" />
+                    Remove
+                  </Button>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
         {/* cart items display end */}
-        <SheetFooter>
+        <SheetFooter className="px-1">
           <SheetClose asChild>
             <Button asChild className="mb-2 mt-5 w-full">
-              <Link href="/">Continue Shopping</Link>
+              <Link href="/">
+                <ShoppingBag className="mr-2 h-4 w-4" />
+                Continue Shopping
+              </Link>
             </Button>
           </SheetClose>
         </SheetFooter>
